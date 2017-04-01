33333bda750429df2c40498b582258cf20ba32a3
jipperspippers <jpaulma23@gmail.com>
2017-04-01 19:00:32 -0400

    Added keypressed in Game.js for User Input

--- game.js
+++ game.js
@@ -1,3 +1,16 @@
-function Game(){
+function Game() {
 
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+
+    } else if (keyCode === RIGHT_ARROW) {
+
+    } else if (keyCode === UP_ARROW) {
+
+    } else if (keyCode === DOWN_ARROW) {
+
+
+    }
 }
