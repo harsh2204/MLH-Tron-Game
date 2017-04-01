048219466f18ad0403918a5207e5550a20390f01
harsh2204 <harshgupta2204@live.com>
2017-04-01 18:32:45 -0400

    Updated the files for initial setup

--- sketch.js
+++ sketch.js
@@ -1,40 +1,12 @@
-var circle = {
-    x: 400,
-    y: 300,
-    dia: 100,
-};
-var vel = {
-    x: 7,
-    y: 0,
-};
-var col = 0;
-var col2 = 0;
-var col3 = 0;
-var jk = 99;
+var gameState; //1-menu, 2-ingame, 3-endgame.
 function setup() {
-    createCanvas(600, 600);
+    gameState=0;
+    var canv = createCanvas(600, 600);
+    var x = (windowWidth - width) / 2;
+    var y = (windowHeight - height) / 2;
+    canv.position(x, y);
 }
-
 function draw() {
-    col = map(circle.x, 0, width, 0, 255);
-    col2 = map(circle.x, 0, width, 255, 0);
-    col3 = map(circle.x, 0, width, 200, 50);
-    background(col);
-    fill(col);
-
-    stroke(col, col2, col3);
-    strokeWeight(4);
-    ellipse(circle.x, circle.y, circle.dia, circle.dia);
-    bounce();
-}
-
-function bounce() {
-    if (circle.x+50 > width || circle.x-50 < 0) {
-        vel.x *= -1
-    }
-    circle.x += vel.x;
-    if (circle.y+50 > height || circle.y-50 < 0) {
-        vel.y *= -1
-    }
-    circle.y += vel.y;
+  
+  background(0);
 }
