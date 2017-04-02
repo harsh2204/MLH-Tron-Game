f74e475e992979a8b06ae4445785494a8e963899
hyucker ducker <jpaulma23@gmail.com>
2017-04-02 08:32:52 -0400

    Menu screen

--- public/menu.js
+++ public/menu.js
@@ -1,11 +1,18 @@
-function Menu(){
-  this.name;
-  this.gameStarted=false;
-  Menu.prototype.setup = function(){
-    this.name= document.querySelector("#username");
-  }
-  Menu.prototype.render = function(){
-
-  }
-  
+function Menu() {
+    var img;
+    Menu.prototype.preload = function() {
+        img = loadImage("title.png");
+        console.log(img);
+    }
+    Menu.prototype.setup = function() {
+        document.querySelector("username");
+    }
+    Menu.prototype.render = function() {
+        image(img, width / 2 - 160, 80);
+        fill(150, 155, 210);
+        rect(width / 2 - 60, 390, 120, 50);
+        fill(255);
+        textSize(40);
+        text("Play", width / 2 - 40, 430);
+    }
 }
