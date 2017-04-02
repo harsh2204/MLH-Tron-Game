14ee1e13be981b141b5e697ad0c322bef5e194c3
harsh2204 <harshgupta2204@live.com>
2017-04-01 20:02:42 -0400

    Defining tron and the menus

--- sketch.js
+++ sketch.js
@@ -1,12 +1,19 @@
 var gameState; //1-menu, 2-ingame, 3-endgame.
 function setup() {
-    gameState=0;
+    gameState=1;
+    var menu = new Menu();
+    var game = new Game();
     var canv = createCanvas(600, 600);
     var x = (windowWidth - width) / 2;
     var y = (windowHeight - height) / 2;
     canv.position(x, y);
 }
 function draw() {
-  
   background(0);
+  if(gameState===1){
+    menu.render();
+  }
+  if(gameState===2){
+    game.render();
+  }
 }
--- tron.js
+++ tron.js
@@ -1,3 +1,29 @@
-function Tron(){
-  
+function Tron(name){
+  this.pos = p5.Vector();
+  this.colour;
+  this.dead;
+  this.id;
+  this.name=name;
+  this.tails =[];
+  this.tail={
+    x,
+    y
+  };
+  Tron.prototype.makeTron = function(data){
+    //data{
+    // x:
+    // y:
+    // colour:
+    // id:
+  // }
+    this.pos.x = data.x;
+    this.pos.y = data.y;
+    this.colour = data.colour;
+    this.id = data.id;
+    this.dead = false;
+  }
+  Tron.prototype.show= function(){
+    
+  }
+
 }
