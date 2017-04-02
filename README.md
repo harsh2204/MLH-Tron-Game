d29aced35b659e6e60e465f37077e89448765962
harsh2204 <harshgupta2204@live.com>
2017-04-02 08:20:46 -0400

    Added node and sockets

--- README.md
+++
@@ -1,2 +0,0 @@
-# MLH-Tron-Game
-A collaborative project for Massey Hacks
--- menu.js
+++
@@ -1,7 +0,0 @@
-function Menu(){
-  Menu.prototype.setup = function(){
-    document.querySelector("username");
-  }
-  Menu.prototype.render = function(){
-  }
-}
